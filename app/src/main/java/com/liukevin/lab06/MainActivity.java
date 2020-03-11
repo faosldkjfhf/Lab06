8de0b5dd6e607092660c083cf8c12710631e94dc
Kevin Liu <dethsandwich@gmail.com>
2020-03-11 13:29:16 -0400

    Lab06 A finished an early layout

--- app/src/main/java/com/liukevin/lab06/MainActivity.java
+++ app/src/main/java/com/liukevin/lab06/MainActivity.java
@@ -3,12 +3,35 @@ package com.liukevin.lab06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image1;
+    ImageView image2;
+    ImageView image3;
+    ImageView image4;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image1 = findViewById(R.id.image1);
+        image2 = findViewById(R.id.image2);
+        image3 = findViewById(R.id.image3);
+        image4 = findViewById(R.id.image4);
+        text = findViewById(R.id.text);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Timer timer = new Timer();
+            }
+        });
     }
 }
