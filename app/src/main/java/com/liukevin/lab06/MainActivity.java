cb57f4a02d7d73b571373552d8f7e4635ef08879
Kevin Liu <dethsandwich@gmail.com>
2020-03-16 11:06:17 -0400

    Lab06 B

--- app/src/main/java/com/liukevin/lab06/MainActivity.java
+++ app/src/main/java/com/liukevin/lab06/MainActivity.java
@@ -1,12 +1,18 @@
 package com.liukevin.lab06;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Button;
+import java.util.Random;
 import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,23 +21,38 @@ public class MainActivity extends AppCompatActivity {
     ImageView image2;
     ImageView image3;
     ImageView image4;
+    SharedPreferences sharedPreferences;
     TextView text;
+    Button quit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
         image1 = findViewById(R.id.image1);
         image2 = findViewById(R.id.image2);
         image3 = findViewById(R.id.image3);
         image4 = findViewById(R.id.image4);
-        text = findViewById(R.id.text);
+        text = findViewById(R.id.title);
+        quit = findViewById(R.id.quit);
+
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+
 
         text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Timer timer = new Timer();
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                TextView textView = (TextView)v;
+                textView.setTextColor(color);
             }
         });
+
     }
 }
