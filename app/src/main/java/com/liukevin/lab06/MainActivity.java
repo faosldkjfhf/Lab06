8f3975c62ecc8e5ab034278b7a637f0f09d0723f
Kevin Liu <dethsandwich@gmail.com>
2020-03-17 09:55:54 -0400

    Lab06 C

--- app/src/main/java/com/liukevin/lab06/MainActivity.java
+++ app/src/main/java/com/liukevin/lab06/MainActivity.java
@@ -21,9 +21,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView image2;
     ImageView image3;
     ImageView image4;
+    TextView text1;
+    TextView text2;
+    TextView text3;
+    TextView text4;
     SharedPreferences sharedPreferences;
     TextView text;
     Button quit;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +40,55 @@ public class MainActivity extends AppCompatActivity {
         image4 = findViewById(R.id.image4);
         text = findViewById(R.id.title);
         quit = findViewById(R.id.quit);
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text2);
+        text3 = findViewById(R.id.text3);
+        text4 = findViewById(R.id.text4);
 
         quit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finishAffinity();
+            }
+        });
+
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor = sharedPreferences.edit();
+                editor.putInt("playboi", sharedPreferences.getInt("playboi", 0) + 1);
+                editor.apply();
+                text1.setText(String.valueOf(sharedPreferences.getInt("playboi", 0)));
+            }
+        });
+
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor = sharedPreferences.edit();
+                editor.putInt("steve", sharedPreferences.getInt("steve", 0) + 1);
+                editor.apply();
+                text2.setText(String.valueOf(sharedPreferences.getInt("steve", 0)));
+            }
+        });
+
+        image3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor = sharedPreferences.edit();
+                editor.putInt("creeper", sharedPreferences.getInt("creeper", 0) + 1);
+                editor.apply();
+                text3.setText(String.valueOf(sharedPreferences.getInt("creeper", 0)));
+            }
+        });
+
+        image4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor = sharedPreferences.edit();
+                editor.putInt("llama", sharedPreferences.getInt("llama", 0) + 1);
+                editor.apply();
+                text4.setText(String.valueOf(sharedPreferences.getInt("llama", 0)));
             }
         });
 
@@ -54,5 +103,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        text1.setText(String.valueOf(sharedPreferences.getInt("playboi", 0)));
+        text2.setText(String.valueOf(sharedPreferences.getInt("steve", 0)));
+        text3.setText(String.valueOf(sharedPreferences.getInt("creeper", 0)));
+        text4.setText(String.valueOf(sharedPreferences.getInt("llama", 0)));
     }
 }
